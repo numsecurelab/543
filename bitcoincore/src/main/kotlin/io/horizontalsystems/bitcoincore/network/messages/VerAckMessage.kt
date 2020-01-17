be8fdf64863969a53ce9866cbdd9482324758caf
numizmatrf <59045411+numizmatrf@users.noreply.github.com>
2020-01-17 21:13:15 +0300

    Update VerAckMessage.kt

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/messages/VerAckMessage.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/messages/VerAckMessage.kt
@@ -1,6 +1,9 @@
 package io.horizontalsystems.bitcoincore.network.messages
+import java.io.ByteArrayInputStream
+import io.horizontalsystems.bitcoincore.io.BitcoinInput
+import io.horizontalsystems.bitcoincore.io.BitcoinOutput
 
-class VerAckMessage : IMessage {
+class VerAckMessage(val vKnownChains: Long) : IMessage {
     override fun toString(): String {
         return "VerAckMessage()"
     }
@@ -10,7 +13,10 @@ class VerAckMessageParser : IMessageParser {
     override val command: String = "verack"
 
     override fun parseMessage(payload: ByteArray): IMessage {
-        return VerAckMessage()
+        BitcoinInput(ByteArrayInputStream(payload)).use { input ->
+            val vKnownChains = input.readUnsignedInt()
+            return PongMessage(vKnownChains)
+        }
     }
 }
 
@@ -22,6 +28,8 @@ class VerAckMessageSerializer : IMessageSerializer {
             return null
         }
 
-        return ByteArray(0)
+        return BitcoinOutput()
+                .writeUnsignedInt(message.vKnownChains)
+                .toByteArray()
     }
 }
