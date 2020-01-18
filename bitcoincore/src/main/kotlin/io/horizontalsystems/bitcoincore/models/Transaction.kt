4b6fa6a404022dc75a15bb6795f8c8ac82d509e8
numizmatrf <59045411+numizmatrf@users.noreply.github.com>
2020-01-19 01:14:38 +0300

    Update Transaction.kt

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/models/Transaction.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/models/Transaction.kt
@@ -35,9 +35,9 @@ open class Transaction {
     var blockHash: ByteArray? = null
 
     var version: Int = 0
-    var lockTime: Long = 0
     var m_nSrcChain: Long = 0
     var m_nDestChain: Long = 0
+    var lockTime: Long = 0
     var timestamp: Long = Date().time / 1000
     var order: Int = 0 // topological order
     var isMine = false
@@ -48,11 +48,12 @@ open class Transaction {
     var conflictingTxHash: ByteArray? = null
 
     constructor()
-    constructor(version: Int = 0, lockTime: Long = 0, m_nSrcChain: Long = 0, m_nDestChain: Long = 0) : this() {
+    constructor(version: Int = 0, m_nSrcChain: Long = 0, m_nDestChain: Long = 0, lockTime: Long = 0) : this() {
         this.version = version
-        this.lockTime = lockTime
         this.m_nSrcChain = m_nSrcChain
         this.m_nDestChain = m_nDestChain
+        this.lockTime = lockTime
+
       
     }
 
