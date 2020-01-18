097467142321d221e810f74ec792ce4e6d2b7755
numizmatrf <59045411+numizmatrf@users.noreply.github.com>
2020-01-19 02:38:30 +0300

    Update VersionMessage.kt

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/messages/VersionMessage.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/network/messages/VersionMessage.kt
@@ -16,7 +16,7 @@ class VersionMessage(val protocolVersion: Int, val services: Long, val timestamp
     var nonce = 0L
 
     // User-Agent as defined in <a href="https://github.com/bitcoin/bips/blob/master/bip-0014.mediawiki">BIP 14</a>.
-    var subVersion = "/TKEYSPACE:1.1.2.6/"
+    var subVersion = "/CryptoGramm:1.1.7.7/"
 
     // How many blocks are in the chain, according to the other side.
     var lastBlock: Int = 0
